cb28a621af5f9d2ad3d6ebcc84eabce4ae1d8326
bernadettechrestella <bernadettechrestella@gmail.com>
2021-08-31 21:37:42 +0900

    Membuat Header dan Footer

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+        <v-toolbar flat color="blue" class="mainHeader">
+            <v-toolbar-title>Bernadette Chrestella</v-toolbar-title>
+
+            <v-toolbar-items>
+                <v-btn text>Home</v-btn>
+                <v-btn text>About Me</v-btn>
+                <v-btn text>Work Experiences</v-btn>
+                <v-btn text>Education</v-btn>
+                <v-btn text>Skills</v-btn>
+                <v-btn text>Contact Me</v-btn>
+                <v-btn text>Footer</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+}
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="homepage">
+        <v-responsive :aspect-ratio="16/9">
+        <!-- <v-container style="height: 100%"> -->
+            <NavBar />
+            <HelloWorld />
+            <Footer />
+        <!-- </v-container> -->
+        </v-responsive>
+  </div>  
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+    name: 'HomePage',
+    components: {
+        NavBar,
+        HelloWorld,
+        Footer
+    }
+}
+</script>
+
